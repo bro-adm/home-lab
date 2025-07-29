0703cbf167a8a88b0364c5bf7a76e48d6fcc7f8b
Liel Bondy <lielbondy@gmail.com>
2025-07-29 04:15:53 +0300

    [] updated shit

--- setup/argo-additional-script.bash
+++ setup/argo-additional-script.bash
@@ -1,1 +1,1 @@
-kubectl create secret generic homelab-github-ssh   --namespace argocd   --from-literal=url=git@github.com:bro-adm/home-lab.git   --from-file=sshPrivateKey=./id_rsa 
+kubectl create secret generic homelab-github-ssh   --namespace argocd   --from-literal=url=git@github.com:bro-adm/home-lab.git   --from-file=sshPrivateKey=/home/bro-adm/.ssh/id_rsa 
